db3230de660c11c52a605987c33d90c83c4ac269
Paweł Jadanowski <pjadanowski@gmail.com>
2024-06-13 17:27:53 +0200

    add endpoint for updating via api call

--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,18 @@ use App\Http\Middleware\SeoAppMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
 // Route::post('sync', [\App\Http\Controllers\Api\SyncController::class, 'update']);
-
 
 Route::group([
     // 'middleware' => 'auth:sanctum' // TODO: make it later
     'middleware' => SeoAppMiddleware::class,
-    'prefix' => '/v1',
+    'prefix'     => '/v1',
 ], function () {
+    Route::post('/app/update', [\App\Http\Controllers\Api\UpdateAppController::class, 'pull']);
     Route::post('logo', [\App\Http\Controllers\Api\LogoController::class, 'update']);
 
     Route::post('articles', [\App\Http\Controllers\Api\ArticleController::class, 'store']);
