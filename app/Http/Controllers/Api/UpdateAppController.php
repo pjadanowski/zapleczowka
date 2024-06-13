e6778179ab1d5f4225370793bb376cdc666b3b18
Paweł Jadanowski <pjadanowski@gmail.com>
2024-06-13 18:32:11 +0200

    added artisan output to response

--- app/Http/Controllers/Api/UpdateAppController.php
+++ app/Http/Controllers/Api/UpdateAppController.php
@@ -6,12 +6,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class UpdateAppController extends Controller
 {
     function pull() {
-        $res = Artisan::call('app:pull');
+         $output = new BufferedOutput;
+         
+        $res = Artisan::call('app:pull', [], $output);
 
-        return new JsonResponse(['status' => $res]);
+        return new JsonResponse(['status' => $res, 'output' => $output->fetch()]);
     }
 }
