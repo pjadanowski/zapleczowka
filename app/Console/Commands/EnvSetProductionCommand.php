15a3743d033aafec8cb4f2c0bae64cd63f8adcfc
Paweł Jadanowski <pjadanowski@gmail.com>
2024-06-19 12:37:44 +0200

    EnvSetProductionCommand

--- app/Console/Commands/EnvSetProductionCommand.php
+++ app/Console/Commands/EnvSetProductionCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Str;
 
 class EnvSetProductionCommand extends Command
 {
@@ -25,13 +27,18 @@ class EnvSetProductionCommand extends Command
      */
     public function handle()
     {
+        $result = Process::run('pwd');
+        $pwd = $result->output();
+        $domain = trim(Str::afterLast($pwd, '/'));
+
         $env = file_get_contents(base_path('.env'));
-    
+
+        $env = preg_replace('/APP_NAME=(.*)/', 'APP_NAME=' . $domain, $env);
         $env = preg_replace('/APP_ENV=(.*)/', 'APP_ENV=production', $env);
         $env = preg_replace('/APP_DEBUG=(.*)/', 'APP_DEBUG=false', $env);
-        $env = preg_replace('/APP_URL=(.*)/', 'APP_URL=', $env);
+        $env = preg_replace('/APP_URL=(.*)/', 'APP_URL=' . $domain, $env);
         $env = preg_replace('/APP_LOCALE=(.*)/', 'APP_LOCALE=pl', $env);
-        
+
         file_put_contents(base_path('.env'), $env);
     }
 }
