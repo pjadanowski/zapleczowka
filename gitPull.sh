fc6fbc7a70ef7e756ea3b0b55bfa7e896cae192e
Paweł Jadanowski <pjadanowski@gmail.com>
2024-07-04 11:26:19 +0200

    git pull fix

--- gitPull.sh
+++ gitPull.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
 ssh-agent bash -c 'ssh-add ~/.ssh/zapleczowka_rsa; git pull'
+
+/opt/alt/php83/usr/bin/php /usr/local/bin/composer install --no-dev
+/opt/alt/php83/usr/bin/php artisan migrate --force
+/opt/alt/php83/usr/bin/php artisan route:clear
+/opt/alt/php83/usr/bin/php artisan view:clear
+/opt/alt/php83/usr/bin/php artisan cache:clear
+/opt/alt/php83/usr/bin/php artisan config:clear
