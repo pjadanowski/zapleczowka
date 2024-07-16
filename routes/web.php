7e8fbbf66781436e0cd15505f4cbec01e6bf57ef
Paweł Jadanowski <pjadanowski@gmail.com>
2024-07-16 21:29:02 +0200

    fix login routes

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,10 @@ use App\Http\Controllers\SitemapController;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__ . '/test.php';
+require __DIR__ . '/auth.php';
 
 Route::get('/', [PageController::class, 'index'])->name('index');
 Route::get('/sitemap', [SitemapController::class, 'show'])->name('sitemap');
 Route::get('/kontakt', [PageController::class, 'contact'])->name('contact');
 Route::get('/category/{categorySlug}', [CategoryArticleController::class, 'show'])->name('category.articles');
 Route::get('/{slug}', [ArticleController::class, 'show'])->name('articles.show');
-
-
-require __DIR__.'/auth.php';
